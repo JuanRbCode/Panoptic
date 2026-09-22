8544f81ab2090ccb782dd07daf21f06acc496535
JuanRbCode <juanrodriguezrb.code@gmail.com>
2026-09-21 20:25:26 -0500

    a ver, correcion y control de audio v2 xd

--- src/components/DeviceCard.jsx
+++ src/components/DeviceCard.jsx
@@ -76,7 +76,7 @@ export default function DeviceCard({ device, frameSrc, onAction, onTalkToggle })
               : 'bg-black text-[#00E5FF] border-[#00e5ff]/30 hover:bg-[#00e5ff]/10'
           }`}
         >
-          📷 CÁ<MARA></MARA> TRASERA
+          📷 CÁMARA TRASERA
         </button>
 
         <button
